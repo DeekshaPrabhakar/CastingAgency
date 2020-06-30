a0571c25f54aa03464b8dc5e662e14420602980b
Deeksha Prabhakar <deekiyengar@gmail.com>
2020-06-30 13:40:09 -0700

    feat: initial commit to test code from heroku starter exercise

---
+++ setup.sh
@@ -0,0 +1,5 @@
+
+export DATABASE_URL='postgres://postgres@localhost:5432/CastingAgency'
+export AUTH0_DOMAIN='deedev.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='https://github.com/DeekshaPrabhakar/CastingAgency'
