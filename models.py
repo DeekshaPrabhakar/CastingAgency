155197ffd3353d6072be5960ef96e47792fdf6dc
Deeksha Prabhakar <deekiyengar@gmail.com>
2020-06-30 13:54:12 -0700

    feat: database using migrations

--- models.py
+++ models.py
@@ -20,7 +20,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    # db.create_all() # will use migrate so commenting out
 
 
 '''
